fa5b2cf4f94e0dc7ee22bb6120db165d0f1a5df2
Eduardo Venegas Rivera <evenegas06@hotmail.com>
2021-03-14 00:34:44 -0600

    Header y avatar

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+const nav = document.getElementById('navbarNav');
+
+//window.onresize = nav;
+//window.onload = nav;
+
+// function nav() {
+//     const width = screen.width;
+//     if (width <= 992) {
+//         nav.classList.remove('justify-content-end');
+//     }
+// }
