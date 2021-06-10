b37ca55ecb3a95fc3ef0f13a3dc92a2605e3988c
Eduardo Venegas <evenegas06@hotmail.com>
2021-06-10 16:37:14 -0500

    Configuración htaccess y se añade sitemap

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,4 @@
 const menu = document.getElementById('btn-abrir-menu');
-
 menu.addEventListener('click', () => {
     document.getElementById('contenedor-menu').classList.toggle('active');
 });
@@ -7,13 +6,11 @@ menu.addEventListener('click', () => {
 const link = document.getElementsByName('link-menu');
 link.forEach(element => {
     element.addEventListener('click', () => {
-        // console.log(element);
         document.getElementById('contenedor-menu').classList.toggle('active');
     });
 });
 
 window.addEventListener('load', () => {
-    // setTimeout(carga, 3000);
     document.getElementById('loading').classList.toggle('hide');
     document.getElementById('main').classList.toggle('hide');
     document.getElementById('footer').classList.toggle('hide');
